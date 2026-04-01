19ffb2fe64aa4c05ad148bc4f7648be61ad7e725
NexoTecnologia <contatonexoti@gmail.com>
2026-03-31 22:16:51 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,52 @@
-// Service Worker para PWA
+// Service Worker para FeiraMini
 const CACHE_NAME = 'feiramini-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/script.js',
-  '/manifest.json'
+  '/FeiraMini/',
+  '/FeiraMini/index.html',
+  '/FeiraMini/css/style.css',
+  '/FeiraMini/js/script.js',
+  '/FeiraMini/manifest.json'
 ];
 
+// Instalação
 self.addEventListener('install', event => {
+  console.log('[Service Worker] Instalando...');
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('[Service Worker] Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => console.log('[Service Worker] Erro ao cachear:', err))
   );
 });
 
+// Ativação
+self.addEventListener('activate', event => {
+  console.log('[Service Worker] Ativando...');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('[Service Worker] Removendo cache antigo:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Intercepta requisições
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
 });
