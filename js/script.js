157520db9899424ea065aad08b2b3e64ee7251ef
NexoTecnologia <contatonexoti@gmail.com>
2026-03-31 21:57:52 -0400

    Update script.js
    Add service worker

--- js/script.js
+++ js/script.js
@@ -3658,3 +3658,14 @@ function adicionarResumoVendasPorFeira(dados) {
         container.appendChild(resumoDiv);
     }
 }
+
+// Registrar Service Worker para PWA
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(registration => {
+            console.log('Service Worker registrado com sucesso!');
+        })
+        .catch(error => {
+            console.log('Falha ao registrar Service Worker:', error);
+        });
+}
